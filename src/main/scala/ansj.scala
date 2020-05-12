607bb09b8b90122a693fd1935b9521f00469ee94
wang1024 <wskqing@qq.com>
2020-05-12 22:49:45 +0800

    update ansj

--- src/main/scala/ansj.scala
+++ src/main/scala/ansj.scala
@@ -5,10 +5,10 @@ class ansj() {
   def ansj_udf = udf((s: String) => ToAnalysis.parse(s))
 }
 
-object ansj
-{
-  def unit_test() = {
+object ansj {
+  def main(args: Array[String]): Unit  = {
     val str: String = "欢迎使用ansj_seg,(ansj中文分词)在这里如果你遇到什么问题都可以联系我.我一定尽我所能.帮助大家.ansj_seg更快,更准,更自由!"
     println(ToAnalysis.parse(str))
   }
+
 }
